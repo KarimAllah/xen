91ac6efb81671dcf1bd45998a3c2483dad5079d2
Thomas Leonard <talex5@gmail.com>
2014-04-10 11:08:49 +0100

    Fixed compiler errors
    xenbus/xenbus.c: In function 'xenbus_transaction_start':
    xenbus/xenbus.c:683:5: error: format '%u' expects argument of type 'unsigned int *', but argument 3 has type 'xenbus_transaction_t *' [-Werror=format=]
         sscanf((char *)(rep + 1), "%u", xbt);
         ^
    xenbus/xenbus.c: In function 'xenbus_get_self_id':
    xenbus/xenbus.c:780:5: error: format '%d' expects argument of type 'int *', but argument 3 has type 'domid_t *' [-Werror=format=]
         sscanf(dom_id, "%d", &ret);

--- extras/mini-os/xenbus/xenbus.c
+++ extras/mini-os/xenbus/xenbus.c
@@ -680,7 +680,9 @@ char *xenbus_transaction_start(xenbus_transaction_t *xbt)
     err = errmsg(rep);
     if (err)
 	return err;
-    sscanf((char *)(rep + 1), "%u", xbt);
+    int trans;
+    sscanf((char *)(rep + 1), "%u", &trans);
+    *xbt = trans;
     free(rep);
     return NULL;
 }
@@ -774,7 +776,7 @@ char* xenbus_printf(xenbus_transaction_t xbt,
 domid_t xenbus_get_self_id(void)
 {
     char *dom_id;
-    domid_t ret;
+    int ret;
 
     BUG_ON(xenbus_read(XBT_NIL, "domid", &dom_id));
     sscanf(dom_id, "%d", &ret);
