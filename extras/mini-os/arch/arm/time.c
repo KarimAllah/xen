6c03f0cc65a268bf4fc59a6f6b839c6e948b1abe
Thomas Leonard <talex5@gmail.com>
2014-04-23 11:05:28 +0100

    Whitespace

--- extras/mini-os/arch/arm/time.c
+++ extras/mini-os/arch/arm/time.c
@@ -21,12 +21,12 @@
 
 /* These are peridically updated in shared_info, and then copied here. */
 struct shadow_time_info {
-	uint64_t tsc_timestamp;     /* TSC at last update of time vals.  */
-	uint64_t system_timestamp;  /* Time, in nanosecs, since boot.    */
-	uint32_t tsc_to_nsec_mul;
-	uint32_t tsc_to_usec_mul;
-	int tsc_shift;
-	uint32_t version;
+    uint64_t tsc_timestamp;     /* TSC at last update of time vals.  */
+    uint64_t system_timestamp;  /* Time, in nanosecs, since boot.    */
+    uint32_t tsc_to_nsec_mul;
+    uint32_t tsc_to_usec_mul;
+    int tsc_shift;
+    uint32_t version;
 };
 static struct timespec shadow_ts;
 static uint32_t shadow_ts_version;
@@ -44,9 +44,9 @@ static struct shadow_time_info shadow;
 
 static inline int time_values_up_to_date(void)
 {
-	struct vcpu_time_info *src = &HYPERVISOR_shared_info->vcpu_info[0].time;
+    struct vcpu_time_info *src = &HYPERVISOR_shared_info->vcpu_info[0].time;
 
-	return (shadow.version == src->version);
+    return (shadow.version == src->version);
 }
 
 
@@ -56,39 +56,39 @@ static inline int time_values_up_to_date(void)
  */
 static inline uint64_t scale_delta(uint64_t delta, uint32_t mul_frac, int shift)
 {
-	BUG();
-	return 0;
+    BUG();
+    return 0;
 }
 
 
 static unsigned long get_nsec_offset(void)
 {
-	return 0;
+    return 0;
 #if FIXME
-	uint64_t now, delta;
-	rdtscll(now);
-	delta = now - shadow.tsc_timestamp;
-	return scale_delta(delta, shadow.tsc_to_nsec_mul, shadow.tsc_shift);
+    uint64_t now, delta;
+    rdtscll(now);
+    delta = now - shadow.tsc_timestamp;
+    return scale_delta(delta, shadow.tsc_to_nsec_mul, shadow.tsc_shift);
 #endif
 }
 
 
 static void get_time_values_from_xen(void)
 {
-	struct vcpu_time_info    *src = &HYPERVISOR_shared_info->vcpu_info[0].time;
+    struct vcpu_time_info    *src = &HYPERVISOR_shared_info->vcpu_info[0].time;
 
- 	do {
-		shadow.version = src->version;
-		rmb();
-		shadow.tsc_timestamp     = src->tsc_timestamp;
-		shadow.system_timestamp  = src->system_time;
-		shadow.tsc_to_nsec_mul   = src->tsc_to_system_mul;
-		shadow.tsc_shift         = src->tsc_shift;
-		rmb();
-	}
-	while ((src->version & 1) | (shadow.version ^ src->version));
+    do {
+        shadow.version = src->version;
+        rmb();
+        shadow.tsc_timestamp     = src->tsc_timestamp;
+        shadow.system_timestamp  = src->system_time;
+        shadow.tsc_to_nsec_mul   = src->tsc_to_system_mul;
+        shadow.tsc_shift         = src->tsc_shift;
+        rmb();
+    }
+    while ((src->version & 1) | (shadow.version ^ src->version));
 
-	shadow.tsc_to_usec_mul = shadow.tsc_to_nsec_mul / 1000;
+    shadow.tsc_to_usec_mul = shadow.tsc_to_nsec_mul / 1000;
 }
 
 
@@ -100,33 +100,33 @@ static void get_time_values_from_xen(void)
  */
 uint64_t monotonic_clock(void)
 {
-	uint64_t time;
-	uint32_t local_time_version;
+    uint64_t time;
+    uint32_t local_time_version;
 
-	do {
-		local_time_version = shadow.version;
-		rmb();
-		time = shadow.system_timestamp + get_nsec_offset();
+    do {
+        local_time_version = shadow.version;
+        rmb();
+        time = shadow.system_timestamp + get_nsec_offset();
         if (!time_values_up_to_date())
-			get_time_values_from_xen();
-		rmb();
-	} while (local_time_version != shadow.version);
+            get_time_values_from_xen();
+        rmb();
+    } while (local_time_version != shadow.version);
 
-	return time;
+    return time;
 }
 
 static void update_wallclock(void)
 {
-	shared_info_t *s = HYPERVISOR_shared_info;
+    shared_info_t *s = HYPERVISOR_shared_info;
 
-	do {
-		shadow_ts_version = s->wc_version;
-		rmb();
-		shadow_ts.tv_sec  = s->wc_sec;
-		shadow_ts.tv_nsec = s->wc_nsec;
-		rmb();
-	}
-	while ((s->wc_version & 1) | (shadow_ts_version ^ s->wc_version));
+    do {
+        shadow_ts_version = s->wc_version;
+        rmb();
+        shadow_ts.tv_sec  = s->wc_sec;
+        shadow_ts.tv_nsec = s->wc_nsec;
+        rmb();
+    }
+    while ((s->wc_version & 1) | (shadow_ts_version ^ s->wc_version));
 }
 
 
@@ -163,60 +163,60 @@ void block_domain(s_time_t until)
  */
 void timer_handler(evtchn_port_t port, struct pt_regs *regs, void *ign)
 {
-	DEBUG("Timer kick\n");
+    DEBUG("Timer kick\n");
     get_time_values_from_xen();
-	update_wallclock();
+    update_wallclock();
 }
 
 #define VTIMER_TICK 0x10000000
 void increment_vtimer_compare(uint64_t inc) {
-	uint32_t x, y;
-	uint64_t value;
-	__asm__ __volatile__("mrrc p15, 1, %0, %1, c14\n"
-			"isb":"=r"(x), "=r"(y));
+    uint32_t x, y;
+    uint64_t value;
+    __asm__ __volatile__("mrrc p15, 1, %0, %1, c14\n"
+            "isb":"=r"(x), "=r"(y));
 
-	// CompareValue = Counter + VTIMER_TICK
-	value = (0xFFFFFFFFFFFFFFFFULL & x) | ((0xFFFFFFFFFFFFFFFFULL & y) << 32);
-	DEBUG("Counter: %llx(x=%x and y=%x)\n", value, x, y);
-	value += inc;
-	DEBUG("New CompareValue : %llx\n", value);
-	x = 0xFFFFFFFFULL & value;
-	y = (value >> 32) & 0xFFFFFFFF;
+    // CompareValue = Counter + VTIMER_TICK
+    value = (0xFFFFFFFFFFFFFFFFULL & x) | ((0xFFFFFFFFFFFFFFFFULL & y) << 32);
+    DEBUG("Counter: %llx(x=%x and y=%x)\n", value, x, y);
+    value += inc;
+    DEBUG("New CompareValue : %llx\n", value);
+    x = 0xFFFFFFFFULL & value;
+    y = (value >> 32) & 0xFFFFFFFF;
 
-	__asm__ __volatile__("mcrr p15, 3, %0, %1, c14\n"
-			"isb"::"r"(x), "r"(y));
+    __asm__ __volatile__("mcrr p15, 3, %0, %1, c14\n"
+            "isb"::"r"(x), "r"(y));
 
-	__asm__ __volatile__("mov %0, #0x1\n"
-				"mcr p15, 0, %0, c14, c3, 1\n" /* Enable timer and unmask the output signal */
-				"isb":"=r"(x));
+    __asm__ __volatile__("mov %0, #0x1\n"
+            "mcr p15, 0, %0, c14, c3, 1\n" /* Enable timer and unmask the output signal */
+            "isb":"=r"(x));
 }
 
 static inline void enable_virtual_timer(void) {
 #if FIXME
-	uint32_t x, y;
-	uint64_t value;
+    uint32_t x, y;
+    uint64_t value;
 
-	__asm__ __volatile__("ldr %0, =0xffffffff\n"
-			"ldr %1, =0xffffffff\n"
-			"dsb\n"
-			"mcrr p15, 3, %0, %1, c14\n" /* set CompareValue to 0x0000ffff 0000ffff */
-			"isb\n"
-			"mov %0, #0x1\n"
-			"mcr p15, 0, %0, c14, c3, 1\n" /* Enable timer and unmask the output signal */
-			"isb":"=r"(x), "=r"(y));
+    __asm__ __volatile__("ldr %0, =0xffffffff\n"
+            "ldr %1, =0xffffffff\n"
+            "dsb\n"
+            "mcrr p15, 3, %0, %1, c14\n" /* set CompareValue to 0x0000ffff 0000ffff */
+            "isb\n"
+            "mov %0, #0x1\n"
+            "mcr p15, 0, %0, c14, c3, 1\n" /* Enable timer and unmask the output signal */
+            "isb":"=r"(x), "=r"(y));
 #else
-	increment_vtimer_compare(VTIMER_TICK);
+    increment_vtimer_compare(VTIMER_TICK);
 #endif
 }
 
 evtchn_port_t timer_port = -1;
 void arch_init_time(void)
 {
-	// FIXME: VIRQ_TIMER isn't supported under ARM, use ARM Generic Timer instead.
+    // FIXME: VIRQ_TIMER isn't supported under ARM, use ARM Generic Timer instead.
     printk("Initialising timer interface\n");
     timer_port = bind_virq(VIRQ_TIMER, (evtchn_handler_t)timer_handler, 0);
     if(timer_port == -1)
-		BUG();
+        BUG();
     unmask_evtchn(timer_port);
 
     enable_virtual_timer();
@@ -224,9 +224,9 @@ void arch_init_time(void)
 
 void arch_fini_time(void)
 {
-	if(timer_port != -1)
-	{
-		mask_evtchn(timer_port);
-		unbind_evtchn(timer_port);
-	}
+    if(timer_port != -1)
+    {
+        mask_evtchn(timer_port);
+        unbind_evtchn(timer_port);
+    }
 }
